c781ba77be37b6c4974de82fb851ca4154285d8b
martha-johnston <martha.johnston@viam.com>
2024-07-31 09:52:17 -0400

    add canary.sh script

---
+++ canary.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ensure neither viam-canary or viam-server are running
+sudo systemctl stop viam-server
+
+# install the RDK server
+sudo curl "https://storage.googleapis.com/packages.viam.com/apps/viam-server/viam-server-latest-$(uname -m).AppImage" -o viam-server
+sudo chmod 755 viam-server
+sudo ./viam-server --aix-install
+
+# start viam-canary service
+sudo systemctl start viam-server
+sleep 120 # allow the server enough time to start
+
+# run the rover canary tests
+cd /home/rover-canary/rover-canary
+sudo go run main.go
+sleep 600 # test should take about 10 mintes
+
+# check for failures
+sudo go run results.go
